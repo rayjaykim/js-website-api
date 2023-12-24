7c8655a619fe6b0ff4d30537f2f984eabe0dcc0c
Raymond Kim <rayjaykim@gmail.com>
2023-12-23 20:28:52 -0500

    changed css, added Sort and beginning the counter for spell type

--- index.js
+++ index.js
@@ -2,20 +2,27 @@ async function createHTML() {
       try {
             const data = await fetch('https://wizard-world-api.herokuapp.com/Spells');
             const spells = await data.json();
-
+            
             const main = document.getElementById('collection');
             const favs = document.getElementById('favorites');
             
+            
 
             const items = spells.slice(0, 30);
-      
+
             items.forEach(item => {
                   const li = document.createElement('div');
-                  li.className = 'item';
+                  const title1 = document.createElement('div');
+                  const text1 = document.createElement('div');
+                  text1.class = 'checkContent';
+                  li.className = `item ${item.type}`;
                   li.id = item.name;
-                  li.innerHTML = `<strong>${item.name}</strong> <br> <br>
-                                          ${item.effect} <br> <br>
-                                          ${item.incantation} <br>`;
+                  title1.innerHTML = `<strong>${item.name}</strong> <br> <br>`;
+                  text1.innerHTML = `Effect: <br> ${item.effect} <br> <br>
+                                    Incantation: <br> ${item.incantation} <br> <br>
+                                    Type: ${item.type}`;
+                  li.append(title1, text1);
+
                   main.append(li);
             });
 
@@ -49,6 +56,42 @@ async function createHTML() {
                   });
             }
             
+            function chantCount() {
+                  const collectionCon = document.querySelector('#collection');
+                  const favoriteCon = document.querySelector('#favorites');
+                  const collectionItems = collectionCon.querySelectorAll('.item');
+                  const favoriteItems = favoriteCon.querySelectorAll('.item');
+
+                  var uniqueSpells = [];
+                  var countArray = [];
+                  var uniqueSpellCount = [];
+                  collectionItems.forEach(element => {
+                        if (!uniqueSpells.includes(element.className.slice(5))) {
+                              uniqueSpells.push(element.className.slice(5));
+                        }
+                        countArray.push(element.className.slice(5));
+                  });
+
+                  uniqueSpellCount.length = uniqueSpells.length;
+                  for (i = 0; i < uniqueSpellCount.length; i++) {
+                        uniqueSpellCount[i] = 0;
+                  }
+
+                  countArray.forEach(element => {
+                        if (uniqueSpells.includes(element)) {
+                              uniqueSpellCount[uniqueSpells.indexOf(element)] += 1;
+                        }
+                  })
+
+                  for (i = 0; i < uniqueSpellCount.length; i++) {
+                        const type = document.createElement('div');
+                        type.innerHTML = `${uniqueSpells[i]}: ${uniqueSpellCount[i]}`
+                  }
+
+                  console.log(uniqueSpells, uniqueSpellCount);
+            }
+            chantCount();
+
             sortBtn.forEach(elem => {
                   elem.addEventListener('click', function() {
                         sortData(elem.dataset.sortdir);
@@ -59,6 +102,7 @@ async function createHTML() {
                   elem.addEventListener('click', function () {
                         const newChild = document.getElementById(this.id);
                         this.parentElement.id === 'collection' ? addCollection(newChild) : remCollection(newChild);
+                        // chantCount();
                   });
             });
       }
